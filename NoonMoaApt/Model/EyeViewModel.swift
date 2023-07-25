9ca54f5401569744fe65b0ffd2bdde7d68bd3312
henry.choi <oreobro@naver.com>
2023-07-25 10:59:50 +0900

    Feat/#16 눈 움직임 부드럽게 수정 완료

--- NoonMoaApt/Model/EyeViewModel.swift
+++ NoonMoaApt/Model/EyeViewModel.swift
@@ -8,8 +8,11 @@ struct EyeViewModel {
     var lookAtPoint: SIMD3 = SIMD3<Float>(0.0, 0.0, 0.0)
     var faceOrientation: SIMD3 = SIMD3<Float>(0.0, 0.0, 0.0)
     
-    var eyeGazeHistory: Array<CGFloat> = []
-    var numberOfHistoryUpdates: Int = 0
+    var lookAtPointHistory: Array<SIMD3<Float>> = []
+    var lookAtPointAverage: SIMD3 = SIMD3<Float>(0.0, 0.0, 0.0)
+    var faceOrientationHistory: Array<SIMD3<Float>> = []
+    var faceOrientationAverage: SIMD3 = SIMD3<Float>(0.0, 0.0, 0.0)
+    var numberOfHistoryUpdates: Int = 10
     
     var savedLookAtPoint: SIMD3<Float> = SIMD3<Float>(0.0, 0.0, 0.0)
     var savedFaceOrientation: SIMD3<Float> = SIMD3<Float>(0.0, 0.0, 0.0)
@@ -22,14 +25,32 @@ struct EyeViewModel {
         //blinkLeft가 오른쪽 눈임...이상하네
         blinkLeft = Float(truncating: blendShapes.first(where: { $0.key == .eyeBlinkLeft })?.value ?? 0)
         blinkRight = Float(truncating: blendShapes.first(where: { $0.key == .eyeBlinkRight })?.value ?? 0)
-
+        
         //얼굴 방향과 상관 없이 x: 눈이 왼쪽일 때 음수, y: 눈이 아래일 때 음수, z: 화면과의 거리
         lookAtPoint = simd_float3(round(faceAnchor.lookAtPoint.x * 1000) / 1000, round(faceAnchor.lookAtPoint.y * 1000) / 1000, round(faceAnchor.lookAtPoint.z * 1000) / 1000)
         
         // .transform의 simd_float4x4의 column 0~3 중 column 2는 화면을 바라보는 축의 값이며, 그 중 x,y값은 사용자가 오른쪽 상단을 바라보았을 때 양수 값을 지닌다.
         faceOrientation = simd_float3(round(faceAnchor.transform.columns.2.x * 1000) / 1000, round(faceAnchor.transform.columns.2.y * 1000) / 1000, round(faceAnchor.transform.columns.2.z * 1000) / 1000)
         
+        //히스토리에 지정된 숫자만큼만 배열에 저장하고, 이를 평균을 내서 값을 돌려준다.
+        lookAtPointHistory.append(lookAtPoint)
+        if lookAtPointHistory.count > numberOfHistoryUpdates {
+            lookAtPointHistory.removeFirst(lookAtPointHistory.count - numberOfHistoryUpdates)
+        }
+        let lookAtPointSuffix: Array<SIMD3<Float>> = lookAtPointHistory.suffix(numberOfHistoryUpdates)
+        lookAtPointAverage = lookAtPointSuffix.averagePoint
+        lookAtPoint = lookAtPointAverage
+        
+        //히스토리에 지정된 숫자만큼만 배열에 저장하고, 이를 평균을 내서 값을 돌려준다.
+        faceOrientationHistory.append(faceOrientation)
+        if faceOrientationHistory.count > numberOfHistoryUpdates {
+            faceOrientationHistory.removeFirst(faceOrientationHistory.count - numberOfHistoryUpdates)
+        }
+        let faceOrientationSuffix: Array<SIMD3<Float>> = faceOrientationHistory.suffix(numberOfHistoryUpdates)
+        faceOrientationAverage = faceOrientationSuffix.averagePoint
+        faceOrientation = faceOrientationAverage
     }
+    
     var isSmiling: Bool {
         smileLeft > 0.3 || smileRight > 0.3
     }
@@ -39,5 +60,22 @@ struct EyeViewModel {
     var isBlinkingRight: Bool {
         blinkRight > 0.8
     }
+    
+}
 
+extension Array where Element == SIMD3<Float> {
+    var averagePoint: SIMD3<Float> {
+        var x: Float = 0
+        var y: Float = 0
+        var z: Float = 0
+
+        for item in self {
+            x += item.x
+            y += item.y
+            z += item.z
+        }
+        
+        let elementCount = Float(self.count)
+        return SIMD3(x: Float(x / elementCount), y: Float(y / elementCount), z: Float(z / elementCount))
+    }
 }
